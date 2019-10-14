de56b6a7a77b639ceaccccdbf363c89955e429ed
Tanner-Mindrum <track2gamesofficial@gmail.com>
2019-10-14 09:48:12 -0700

    Added option 3 to java

--- JavaLibraryCodeSql.java
+++ JavaLibraryCodeSql.java
@@ -87,7 +87,7 @@ public class JavaLibraryCodeSql { // JDBC driver name and database URL
                 //String subject = rs.getString("subject");
 
                 //Display values
-                System.out.println("Group #" + groupCount + ": " + groupName);
+                System.out.println("Writing group #" + groupCount + ": " + groupName);
                 //System.out.print(", Head Writer: " + headWriter);
                 //System.out.print(", Year Made: " + year);
                 //System.out.println(", Subject: " + subject);
@@ -128,6 +128,65 @@ public class JavaLibraryCodeSql { // JDBC driver name and database URL
         
         //List all Publishers -----------------------------------------------------------
 	else if (Option == 3) {
+            
+            try{
+                
+            //STEP 2: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            
+            String sql;
+            sql = "SELECT publisherName FROM Publisher";
+            ResultSet rs = stmt.executeQuery(sql);
+            
+            //STEP 5: Extract data from result set
+            int pubCount = 1;
+            while(rs.next()){
+                //Retrieve by column name
+                String pubName  = rs.getString("publisherName");
+                //String headWriter = rs.getString("headWriter");
+                //String year = rs.getString("yearFormed");
+                //String subject = rs.getString("subject");
+
+                //Display values
+                System.out.println("Publisher #" + pubCount + ": " + pubName);
+                //System.out.print(", Head Writer: " + headWriter);
+                //System.out.print(", Year Made: " + year);
+                //System.out.println(", Subject: " + subject);
+                pubCount++;
+            }
+            System.out.println();
+            //STEP 6: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+            }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+            }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+            }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+            }//end try
 			
 	}
         
