51341fd56bc39f43973f369429488c97cc96a3ba
stevenone11 <stevencenteno13@gmail.com>
2019-10-14 09:19:25 -0700

    Update JavaLibraryCodeSql.java
    java errors plz help

--- JavaLibraryCodeSql.java
+++ JavaLibraryCodeSql.java
@@ -163,6 +163,9 @@ public class JavaLibraryCodeSql { // JDBC driver name and database URL
       System.out.println("Connecting to database...");
       conn = DriverManager.getConnection(DB_URL,USER,PASS);
       
+      //STEP 4: Execute a query
+      System.out.println("Creating statement...");
+      stmt = conn.createStatement();
       
       boolean inMainMenu = true;
       int MenuSelection = 0;
@@ -185,12 +188,26 @@ public class JavaLibraryCodeSql { // JDBC driver name and database URL
             inMainMenu = false;
 	}
       }
-    }catch(SQLException se){
-        //Handle errors for JDBC
-        se.printStackTrace();
-    }catch(Exception e){
-        //Handle errors for Class.forName
-        e.printStackTrace();
-    }
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      //finally block used to close resources
+      try{
+         if(stmt!=null)
+            stmt.close();
+      }catch(SQLException se2){
+      }// nothing we can do
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+   }//end try
+   System.out.println("Goodbye!");
 }//end main
 }
